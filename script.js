3faaff55e89df6bc83127537affc3965d5294de2
elaintran <elain.ttran@gmail.com>
2018-09-28 15:52:30 -0500

    limit options and moved position of autocomplete

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-$(".query").autocomplete({
-    source: function(request, response) {
-        $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                'action': "opensearch",
-                'format': "json",
-                'search': request.term
-            },
-            success: function(data) {
-                response(data[1]);
-            }
-        });
-        var results = $.ui.autocomplete.filter($.ajax, request.term);
-        response(results.slice(0, 6));
+$('.query').autocomplete({
+    source: function(request, response){
+      $.ajax({
+        url: "https://en.wikipedia.org/w/api.php",
+        dataType: "jsonp",
+        data: {
+          "action": "opensearch",
+          "format": "json",
+          "limit": 5,
+          "search": request.term
+        },
+        success: function(data){
+          response(data[1]);
+        }
+      });
+    },
+    //set limits to search
+    select: function(event, ui){
+      if (event.which === 1){
+        search(ui.item.value);
+        $(".query").val(ui.item.value);
+      }
+    },
+    //moves position of autocomplete
+    position: {
+        my: "left-14 top+15",
     }
-});
+  });
