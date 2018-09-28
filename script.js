f6258a509bc1e013c15d4c6959a250afeb52486e
elaintran <elain.ttran@gmail.com>
2018-09-27 22:59:50 -0500

    updated with autocomplete

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".query").autocomplete({
+    source: function(request, response) {
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function(data) {
+                response(data[1]);
+            }
+        });
+        var results = $.ui.autocomplete.filter($.ajax, request.term);
+        response(results.slice(0, 6));
+    }
+});
