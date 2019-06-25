eca00ecc1a88d26610bcb46521eda0ddf6ac613c
elaintran <elain.ttran@gmail.com>
2019-06-24 19:51:55 -0500

    reset styling, rewrote js elements into jquery, broke down appends

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-var resultContainer = document.querySelector("#result-container");
-var query = document.querySelector("#query");
-var searchButton = document.querySelector("#search-button");
-var form = document.querySelector("#search-input");
-var container = document.querySelector(".container");
-var title = document.querySelector(".title");
-var footer = document.querySelector("footer");
-
 //allows cursor focus on search input upon page load
 window.onload = function() {
-  query.focus();
+  $("#query").focus();
 };
 
-//submit form
-form.addEventListener("submit", submitInput);
+//form submit
+$("form").on("submit", submitInput);
 
 function submitInput(event) {
   //prevent page reload upon submit
   event.preventDefault();
-  //takes search input
-  var input = query.value;
-  //removes white space in search input
-  var searchQuery = input.trim();
+  //takes search input and trim off white space from the edges
+  var searchQuery = query.value.trim();
   //submits if length more than 0
   if (searchQuery.length > 0) {
-  //display result-container
-  resultContainer.style.display = "block";
-  footer.style.display = "block";
-  //change css on search container
-  container.style.height = 0;
-  title.style.marginTop = "100px";
-  //displays results
-  runSearch(searchQuery);
-  //closes autocomplete on submit
-  $("#query").autocomplete("close");
-  }
-  else {
-    return false;
+    //display result-container
+    $("#result-container").css("display", "block");
+    $("footer").css("display", "block");
+    //change css on search container
+    $(".container").css("height", 0);
+    $(".title").css("margin-top", "100px");
+    //displays results
+    runSearch(searchQuery);
+    //closes autocomplete on submit
+    $("#query").autocomplete("close");
   }
 }
 
@@ -74,21 +61,35 @@ function runSearch(searchInput) {
   //gets Wikipedia information
   $.getJSON (wikiURL, function(data) {
     console.log(data);
-    $(".results").html("");
-    //put object in array
-    var resultArray = Object.keys(data);
+    $(".results").empty();
     //if no matching results, display error
-    if (resultArray.length < 3) {
-      $(".results").append("<div class='error'><p>Sorry, we could not find any results matching <b>" + searchInput + "</b>.</p><ul style='list-style-type:disc'><li>Make sure all words are spelled correctly.</li><li>Try different keywords.</li><li>Try more general keywords.</li></ul></div>");
-      footer.style.position = "absolute";
-      footer.style.width = "100%";
+    if (data.query === undefined) {
+      var error = $("<div>").addClass("error");
+      var errorMessage = $("<p>").text("Sorry, we could not find any results matching " + searchInput + ".");
+      var errorOptions = $("<ul>").attr("style", "list-style-type:disc");
+      var liOne = $("<li>").text("Make sure all words are spelled correctly.");
+      var liTwo = $("<li>").text("Try different keywords.");
+      var liThree = $("<li>").text("Try more general keywords.");
+      errorOptions.append(liOne).append(liTwo).append(liThree);
+      error.append(errorMessage).append(errorOptions);
+      $(".results").append(error);
+      $("footer").css({
+        "position": "absolute",
+        "width": "100%"
+      });
     }
     //loops through and display results
     else {
       for (var i in data.query.pages) {
-        $(".results").append("<a href ='https://en.wikipedia.org/wiki?curid=" + data.query.pages[i].pageid + "'><div class='result-list'><div class='result-title'>" + data.query.pages[i].title + "</div><p>" + data.query.pages[i].extract + "</p></div></a>");
-        }
-      footer.style.position = "";
+        var wikiLink = $("<a>").attr("href", "https://en.wikipedia.org/wiki?curid=" + data.query.pages[i].pageid);
+        var resultList = $("<div>").addClass("result-list");
+        var title = $("<div>").addClass("result-title").text(data.query.pages[i].title);
+        var content = $("<p>").text(data.query.pages[i].extract);
+        resultList.append(title).append(content);
+        wikiLink.append(resultList);
+        $(".results").append(wikiLink);
+      }
+      $("footer").css("position", "static");
     }
   });
 }
